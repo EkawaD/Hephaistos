8fe37bf2d640cb2db8099af57396706a5b970d0a
EkawaD <steo.ederhy@gmail.com>
2022-11-10 01:16:30 +0100

    fix prod

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@ import { createGetInitialProps } from '@mantine/next';
 
 const getInitialProps = createGetInitialProps();
 
+
 export default class _Document extends Document {
   static getInitialProps = getInitialProps;
 }
