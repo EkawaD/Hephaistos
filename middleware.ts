3fab9491ded99e77e20be1fe94f9ce536230b279
EkawaD <steo.ederhy@gmail.com>
2022-11-12 16:59:40 +0100

    test middleware

--- middleware.ts
+++ middleware.ts
@@ -20,14 +20,15 @@ export default withAuth({
             // data based off of its value. This middleware simply redirects unauthenticated users to the login
             // page (and sets a callbackUrl) for all routes, except static files, api routes, Next.js internals,
             // and the whitelisted anonymousRoutes above.
-            return Boolean(
-                req.cookies.get('next-auth.session-token') || // check if there's a token
-                    pathname.startsWith('/_next') || // exclude Next.js internals
-                    pathname.startsWith('/api') || //  exclude all API routes
-                    pathname.startsWith('/static') || // exclude static files
-                    publicFileRegex.test(pathname) || // exclude all files in the public folder
-                    anonymousRoutes.includes(pathname)
-            );
+            // return Boolean(
+            //     req.cookies.get('next-auth.session-token') || // check if there's a token
+            //         pathname.startsWith('/_next') || // exclude Next.js internals
+            //         pathname.startsWith('/api') || //  exclude all API routes
+            //         pathname.startsWith('/static') || // exclude static files
+            //         publicFileRegex.test(pathname) || // exclude all files in the public folder
+            //         anonymousRoutes.includes(pathname)
+            // );
+            return true;
         },
     },
     // If you have custom pages like I do, these should be whitelisted!
